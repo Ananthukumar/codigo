ef32782a51c83b78d694eee19fc8b9083a905ac2
ananthu.k <ananthu.k@iroidtechnologies.com>
2022-04-21 18:08:34 +0530

    hh

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
                 <div className="header_form">
                     <form action="" className="search__form ">
                         <input type="text" className="form-control" name="" id="" placeholder="Search Your Places" />
-                       <span className="search"><svg width="20" height="20" class="DFW_E nT46U VETef" viewBox="0 0 32 32" version="1.1" aria-hidden="false"><path d="M22 20c1.2-1.6 2-3.7 2-6 0-5.5-4.5-10-10-10S4 8.5 4 14s4.5 10 10 10c2.3 0 4.3-.7 6-2l6.1 6 1.9-2-6-6zm-8 1.3c-4 0-7.3-3.3-7.3-7.3S10 6.7 14 6.7s7.3 3.3 7.3 7.3-3.3 7.3-7.3 7.3z"></path></svg></span>
+                       <span className="search"><svg width="18" height="18" class="DFW_E nT46U VETef" viewBox="0 0 32 32" version="1.1" aria-hidden="false"><path d="M22 20c1.2-1.6 2-3.7 2-6 0-5.5-4.5-10-10-10S4 8.5 4 14s4.5 10 10 10c2.3 0 4.3-.7 6-2l6.1 6 1.9-2-6-6zm-8 1.3c-4 0-7.3-3.3-7.3-7.3S10 6.7 14 6.7s7.3 3.3 7.3 7.3-3.3 7.3-7.3 7.3z"></path></svg></span>
                     </form>
                 </div>
                 <div className="home__left">
@@ -23,7 +23,7 @@ function Header() {
                 </div>
 
                 <div>
-                    <button className="btn__cus"> Login / sign up</button>
+                    <button className="btn__cus"><a href="/Login" className="text-decoration-none text-black">Login / sign up</a></button>
                 </div>
 
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,15 +2,34 @@ import React from "react";
 
 function login() {
     return(
-        <div className="login__wrap">
-            <div className="row">
-                <div className="col-lg-4">
-                    <div className="image__wrap">
-                        <img src="" alt="" />s
-                    </div>
-                </div>
-                <div className="col-lg-6">s</div>
+        <div className="" style={{position:"relative"}}>
+        <div className="login__wrapper">
+                    <div className="login__wrap">
+                        {/* <div className="text-center mb-4">
+                        <a href="/" className="logo"><svg width="50" height="50" class="hic6U" viewBox="0 0 32 32" version="1.1" aria-labelledby="unsplash-home" aria-hidden="false"><title id="unsplash-home">
+                </title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg></a>
+                        </div> */}
+                        <h2 className="text-center">LOG IN</h2>
+                        <div className="form__group mb-3">
+                            <input type="text" className="form__control" placeholder="First Name" />
+                        </div>
+                        <div className="form__group mb-3">
+                            <input type="text" className="form__control" placeholder="First Name" />
+                        </div>
+                        <div className="d-grid">
+                            <button className="btn__primary">Submit</button>
+                        </div>
+                        <p className="text-center py-4">Don't have an account? <a href=""> Join</a></p> 
             </div>
+
+          
+        </div>
+
+        {/* <div className="login__loop">
+                    <div className="text">
+                        Provoking the intrepid spirit in everyday people that propels ambition into fruition.
+                    </div>
+            </div> */}
         </div>
     )
 }
